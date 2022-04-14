32523080a0cc3a5071e815b55f4c631d81919b36
MichNunes <89074972+MichNunes@users.noreply.github.com>
2022-04-14 20:36:31 -0300

    cadastro js + small changes

--- cadastro/assets/js/cadastro.js
+++ cadastro/assets/js/cadastro.js
@@ -7,11 +7,17 @@ let botaoCriarConta = document.getElementById("criarContaCadastro");
 
 function validatePassword(){
     if(campoSenhaCadastro.value != campoRepetirSenhaCadastro.value) {
-        campoRepetirSenhaCadastro.setCustomValidity("Senhas diferentes!")
         return false;
     } else {
-        campoRepetirSenhaCadastro.setCustomValidity('');
         return true
+    }
+}
+
+function validaEmailRecebido(emailRecebido) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailRecebido)) {
+      return true;
+    } else {
+      return false;
     }
 }
 
@@ -33,18 +39,40 @@ function cadastroSucesso() {
     botao[0].addEventListener('click', () => {window.location.pathname = "/front2-checkpoint2/login/login.html"})
 }
 
-function cadastroErro(statusRecebido) {
-    console.log("Erro ao cadastrar");
-    console.log(statusRecebido);
+function cadastroErro(error) {
+    if (error.status == 400) {
+    console.log("nada de bad request não, email já cadastrado")};
 }
 
 botaoCriarConta.addEventListener('click', evento => {
+
     evento.preventDefault();
     var senhaValidada = validatePassword()
+    var emailValidado = validaEmailRecebido(campoEmailCadastro.value);
 
     if (campoNomeCadastro.value != "" && campoSobrenomeCadastro.value != "" &&
         campoEmailCadastro.value != "" && campoSenhaCadastro.value != "" &&
-        campoRepetirSenhaCadastro.value != "" && senhaValidada) {
+        campoRepetirSenhaCadastro.value != "") {
+
+        if (senhaValidada === false) {
+            cuteAlert({
+                type: "warning",
+                title: "senhas não conferem",
+                message: "sua senha não é igual a sua confirmação de senha",
+                buttonText: "okay"
+            })
+            return
+        }
+
+        if (emailValidado === false) {
+            cuteAlert({
+                type: "warning",
+                title: "email invalido",
+                message: "insira um email valido",
+                buttonText: "okay"
+            })
+            return
+        }
 
         usuarioObjetoCadastro.firstName = campoNomeCadastro.value;
         usuarioObjetoCadastro.lastName = campoSobrenomeCadastro.value;
@@ -63,18 +91,57 @@ botaoCriarConta.addEventListener('click', evento => {
 
         fetch("https://ctd-todo-api.herokuapp.com/v1/users", configuracaoRequisicao)
         .then((response) => {
+
+            if (response.status = 400) {
+                console.log('nada de bad request não, email fornecido já está cadastrado')
+            }
+
+            if (!!response) {return response}
+        
+        })
+        .then(function (response) {
+
+            if (response.status == 400) {
+                cuteAlert({
+                    type: "warning",
+                    title: "email já cadastrado",
+                    message: "criar uma nova conta ou fazer login?",
+                    buttonText: "nova conta"
+                })
+                
+                alertBody = [];
+
+                var alertBody = document.getElementsByClassName('alert-body')
+
+                var botao2 = document.createElement('button')
+                botao2.classList.add ('alert-button')
+                botao2.classList.add ('success-bg')
+                botao2.classList.add ('success-btn')
+         
+                botao2.style.marginTop = '10px'
+                botao2.innerHTML = 'fazer login'
+
+                alertBody[0].appendChild(botao2)
+                botao2.addEventListener('click', () => {window.location.pathname = "/front2-checkpoint2/login/login.html"})
+                campoEmailCadastro.value = ""
+            }
+
             if (response.status == 201) {
-                return response.json()
-            } throw response;
+            cadastroSucesso()}
+
         })
-        .then(function () {
-            cadastroSucesso()
-        })
-        .catch(error => {
-            cadastroErro(error)
+        .catch(response => {
+            console.log(response)
+            cadastroErro(response)
         });
     } else {
-        alert("Todos os campos devem ser preenchidos para que possa prosseguir")
+        cuteAlert({
+            type: "error",
+            title: "campos incompletos",
+            message: "todos os campos devem ser preenchidos para realizar o cadastro",
+            buttonText: "okay"
+        })
+          return
     }
 });
 
